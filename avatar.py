ee82dfea5c9d0a35585d6c9cc199912a43096aa0
bonya-boop <amirbekovabonya@gmail.com>
2026-02-09 17:36:54 +0500

    d

--- avatar.py
+++ avatar.py
@@ -1,8 +1,15 @@
 
-
+name = input('name - ')
 print('Hello Adventurer, Lets create your avatar!')
 #Race
 print('what is the race of your avatar? ')
-print = ('Elf, Dwarf, Sage, Dragon, Mermaid, Hunter')
+print = ('Elf, Dragon, Hunter')
 race = input('Please choose: ')
-
+if race == 'Elf':
+    print(f'Hello, {name} the Elf')
+elif race == 'Dragon':
+    print(f'Hello, {name} the Dragon')
+elif race == 'Hunter':
+    print(f'Hello, {name} the Hunter')
+else:
+    print('No you are banned now')
