73e54d3f299692891c8ebe21c53523bf8fc05c6a
bonya-boop <amirbekovabonya@gmail.com>
2026-02-09 17:28:09 +0500

    first step

---
+++ avatar.py
@@ -0,0 +1,8 @@
+
+
+print('Hello Adventurer, Lets create your avatar!')
+#Race
+print('what is the race of your avatar? ')
+print = ('Elf, Dwarf, Sage, Dragon, Mermaid, Hunter')
+race = input('Please choose: ')
+
---
+++ first.py
@@ -0,0 +1,9 @@
+#first program greats
+# asks name
+# and helps to male an avatar
+#
+#
+
+name = input('Name - ')
+print('Hi', name)
+
